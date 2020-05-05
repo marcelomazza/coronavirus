b5f02710039d20a564dbfa218a46ff9745055d52
Marcelo Mazza <ux@marcelomazza.com.ar>
2020-05-05 00:35:06 -0300

    Add CovidChart

---
+++ src/CovidChart.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from 'react';
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts';
+
+function CovidChart() {
+  const [items, setItems] = useState([]);
+
+  const requestOptions = {
+    method: 'GET',
+    redirect: 'follow',
+    from: '2020-03-01T00:00:00Z'
+  };
+
+  useEffect(() => {
+    fetch("https://api.covid19api.com/total/country/italy", requestOptions)
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setItems(result.map(function(d, i) {
+            let delta = d.Confirmed - d.Recovered - d.Deaths;
+
+            return {
+              date: d.Date,
+              delta: delta,
+            };
+          }));
+        }
+      )
+  }, []);
+
+  return (
+    <LineChart width={900} height={600} data={items} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
+      <Line type="basis" dataKey="delta" stroke="green" dot={false} />
+      <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+      <XAxis dataKey="date" />
+      <YAxis />
+      <Tooltip />
+    </LineChart>
+  );
+}
+
+export default CovidChart;
