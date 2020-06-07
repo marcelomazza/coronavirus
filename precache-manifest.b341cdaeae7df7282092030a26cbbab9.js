078a185450135c859ad9349240fbc8d3fb231023
Marcelo Mazza <ux@marcelomazza.com.ar>
2020-06-07 20:23:18 -0300

    Updates

---
+++ precache-manifest.b341cdaeae7df7282092030a26cbbab9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcce84257c9b8781b591ce3da204b21d",
+    "url": "/coronavirus/index.html"
+  },
+  {
+    "revision": "7ccf78c3950f4a77bd4e",
+    "url": "/coronavirus/static/css/main.00092e7b.chunk.css"
+  },
+  {
+    "revision": "5a2c2d0e52015f7ab429",
+    "url": "/coronavirus/static/js/2.fe6a204e.chunk.js"
+  },
+  {
+    "revision": "aa754ba9f73776ab5291549b6092b0f0",
+    "url": "/coronavirus/static/js/2.fe6a204e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ccf78c3950f4a77bd4e",
+    "url": "/coronavirus/static/js/main.dd456339.chunk.js"
+  },
+  {
+    "revision": "d0e8ebf6b6c6c4119fba",
+    "url": "/coronavirus/static/js/runtime-main.11f5bc70.js"
+  }
+]);
