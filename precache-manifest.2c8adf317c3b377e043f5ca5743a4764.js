fa417c4168b80757d839ecb963ca550a00c85756
Marcelo Mazza <ux@marcelomazza.com.ar>
2020-06-07 21:09:54 -0300

    Updates

--- precache-manifest.2c8adf317c3b377e043f5ca5743a4764.js
+++ precache-manifest.2c8adf317c3b377e043f5ca5743a4764.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3d7909e5faca5c74fc65488a309275ed",
+    "revision": "3b904f59e67a348e03c4f39e829711ad",
     "url": "/coronavirus/index.html"
   },
   {
-    "revision": "325f178cdb09287e9fb3",
+    "revision": "a367f0970211c0d1252c",
     "url": "/coronavirus/static/css/main.2d3d803e.chunk.css"
   },
   {
-    "revision": "5a2c2d0e52015f7ab429",
-    "url": "/coronavirus/static/js/2.fe6a204e.chunk.js"
+    "revision": "02a701abd65a550798ae",
+    "url": "/coronavirus/static/js/2.aed6c599.chunk.js"
   },
   {
     "revision": "aa754ba9f73776ab5291549b6092b0f0",
-    "url": "/coronavirus/static/js/2.fe6a204e.chunk.js.LICENSE.txt"
+    "url": "/coronavirus/static/js/2.aed6c599.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "325f178cdb09287e9fb3",
-    "url": "/coronavirus/static/js/main.5d602a84.chunk.js"
+    "revision": "a367f0970211c0d1252c",
+    "url": "/coronavirus/static/js/main.1dc63ead.chunk.js"
   },
   {
     "revision": "d0e8ebf6b6c6c4119fba",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/coronavirus/precache-manifest.452aca08475c083b00c57975c6287827.js"
+  "/coronavirus/precache-manifest.2c8adf317c3b377e043f5ca5743a4764.js"
 );
 
 self.addEventListener('message', (event) => {
