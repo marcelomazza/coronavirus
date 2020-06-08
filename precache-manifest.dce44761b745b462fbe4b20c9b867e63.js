facf426e2928a03df7f443d6f88b7e6082c2703a
Marcelo Mazza <ux@marcelomazza.com.ar>
2020-06-07 22:42:13 -0300

    Updates

--- precache-manifest.dce44761b745b462fbe4b20c9b867e63.js
+++ precache-manifest.dce44761b745b462fbe4b20c9b867e63.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "097b3801ca2dafe13dd38093228757cc",
+    "revision": "839719ad951a6e4272963052b1685d66",
     "url": "/coronavirus/index.html"
   },
   {
-    "revision": "9acb8405a32ecb4b8f2f",
-    "url": "/coronavirus/static/css/main.02dd49db.chunk.css"
+    "revision": "948c44f7d9ad0a30adfe",
+    "url": "/coronavirus/static/css/main.196c9285.chunk.css"
   },
   {
     "revision": "af08785e893c2cb73a63",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/coronavirus/static/js/2.0fd5f710.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "9acb8405a32ecb4b8f2f",
-    "url": "/coronavirus/static/js/main.8ddb58be.chunk.js"
+    "revision": "948c44f7d9ad0a30adfe",
+    "url": "/coronavirus/static/js/main.2f2042f3.chunk.js"
   },
   {
     "revision": "d0e8ebf6b6c6c4119fba",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/coronavirus/precache-manifest.52d9f02ff202c33b827fc5ef764137c9.js"
+  "/coronavirus/precache-manifest.dce44761b745b462fbe4b20c9b867e63.js"
 );
 
 self.addEventListener('message', (event) => {
