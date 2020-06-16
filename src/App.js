546d6898bbe3ba5b0a0e129a147a853f714b06f6
Marcelo Mazza <ux@marcelomazza.com.ar>
2020-06-15 22:57:26 -0300

    Move countries fetching to App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useQueryParams, StringParam, withDefault } from 'use-query-params';
 import "./App.scss"
 import Container from "./Container"
@@ -12,11 +12,34 @@ function App() {
     Country: withDefault(StringParam, 'China')
   });
 
+  const [apiCountries, setApiCountries] = useState([]);
+
+  useEffect(() => {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow',
+      from: '2020-03-01T00:00:00Z',
+    };
+
+    fetch("https://api.covid19api.com/countries", requestOptions)
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setApiCountries(result.sort((a, b) => a.Country.localeCompare(b.Country)));
+        }
+      ).catch(
+        (error) => {
+          console.log(error)
+        }
+      )
+  }, []);
+
+
   return (
     <Container>
       <CovidHeader />
       <CovidChart country={country} />
-      <CountrySelector country={country} setCountry={setCountry} style={{ 'flexShrink': 1 }}/>
+      <CountrySelector country={country} apiCountries={apiCountries} setCountry={setCountry} style={{ 'flexShrink': 1 }}/>
     </Container>
   );
 }
--- src/CountrySelector.js
+++ src/CountrySelector.js
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import CountrySelectorStyles from "./CountrySelector.module.scss"
 
-function CountrySelector({ country, setCountry }) {
+function CountrySelector({ country, setCountry, apiCountries }) {
   const selectedCountries = [
     { Slug: 'argentina', Country: 'Argentina' },
     { Slug: 'brazil', Country: 'Brazil' },
@@ -15,28 +15,7 @@ function CountrySelector({ country, setCountry }) {
     { Slug: 'uruguay', Country: 'Uruguay' },
   ];
 
-  const [apiCountries, setApiCountries] = useState([]);
   const [showSelectedCountries, setshowSelectedCountries] = useState(true);
-
-  useEffect(() => {
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow',
-      from: '2020-03-01T00:00:00Z',
-    };
-
-    fetch("https://api.covid19api.com/countries", requestOptions)
-      .then(res => res.json())
-      .then(
-        (result) => {
-          setApiCountries(result.sort((a, b) => a.Country.localeCompare(b.Country)));
-        }
-      ).catch(
-        (error) => {
-          console.log(error)
-        }
-      )
-  }, []);
 
   return (
     <div className={CountrySelectorStyles.container}>
